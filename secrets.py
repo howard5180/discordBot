d27e2efe1320d769a381bbd06e623a1eade4192e
howard5180 <39892311+howard5180@users.noreply.github.com>
2018-12-17 08:15:30 +0800

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,19 +1,23 @@
 import pymysql.cursors
 import pymysql
 import re
+import os
+    
+access_token = os.environ["ACCESS_TOKEN"]
+db_name      = os.environ["DB_NAME"]
+db_password  = os.environ["DB_PASSWORD"]
+db_username = os.environ["DB_USERNAME"]
+db_hostname     = os.environ["DB_HOSTNAME"]
 
 def getConnection():
-    connection = pymysql.connect(host='localhost',
-                                user='root',
-                                password='',
-                                db='karyu_db',
+    connection = pymysql.connect(host=db_hostname,
+                                user=db_username,
+                                password=db_password,
+                                db=db_name,
                                 charset='utf8mb4',
                                 cursorclass=pymysql.cursors.DictCursor)
     return connection
 
-#connection: host = 'localhost', user = 'root', pw = '', db = 'karyu_db'
-#venom server: host = 'sql9.freemysqlhosting.net', user and db = 'sql9262534', pw = 'BYwDPuPyas'
-
 def getToken():
-    token = "NDY4MzIyMzYwNDU4NzM5NzEy.Dja62g.bh354nnwR7dGif4Id480Xl71Cmw"
+    token = access_token
     return token
