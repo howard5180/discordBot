9de1e6bd4360e12c11a1bce294f5c4811ca1fad9
Howard Chan <howard.chan@prenetics.com>
2019-09-18 17:07:55 -0400

    adding more glossary

--- glossary.py
+++ glossary.py
@@ -9,7 +9,13 @@ glossary_list = {"hc":"Refers to Hero's Crusade, one of the new arena mode where
 "magi":"basically skills, can equip on gear slots according to the shape of the magi slots on the gear. There are 4 types of magi: attack (red), support (blue), heal (green) and passive (yellow). When ppl says `rgb` or `rrb` it refers to the color of the magi a gear can equip.",
 "crystal":"an item that allows you to add an extra line of ability to armor gears that the crystal carries.",
 "br":"Refers to Battle Royale, an event where useful loots (such as featured tablet and ingots) drop when you kill the unique behemoth",
-"pr":"Means Perfect Roll, refers to the state where you roll the max available unique ability for the gear"}
+"pr":"Means Perfect Roll, refers to the state where you roll the max available unique ability for the gear",
+"lb":"Means Limit Break. LB will give more stats and an extra magi slot on the 4th one",
+"mlb":"Means Max Limit Break, refers to the state you fully LB a gear",
+"rrb":"while it mostly refers to the color of magi slot on a weapon, it also means Rage Resistance Battle, a form of defense battle",
+"expo":"Means Expedition, an event where you can get crystals",
+"awakening":"You will be able to awaken magi when it reaches max level (except for passive). Each +1 awakening will reduce the max cd of that magi by 2%. The max awakening level you can get on a magi is +10, which reduces -20\% of the max CD. Awakening will not affect the damage of the magi, but for SS meteora series, the awakening level affects the damage of status ailment it causes.",
+"burn":"A status ailment. When it works on player, you keep receiving damage at a high frequency until it wears off. Rolling (dodging) will speed up the wearing off process. When it works on behemoth, it will keeping dealing DoT for a period of time, however only works on earth element behemoths."}
 
 def get_glossary():
     return glossary_list
